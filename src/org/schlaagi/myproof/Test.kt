5a887eb9f0a8050923d5198289dcb264c4efcf23
Schlaagi <30042757+Schlaagi@users.noreply.github.com>
2019-10-17 18:45:55 +0200

    Example: Church numerals
    Signed-off-by: Schlaagi <30042757+Schlaagi@users.noreply.github.com>

--- src/org/schlaagi/myproof/Test.kt
+++ src/org/schlaagi/myproof/Test.kt
@@ -1,5 +1,8 @@
 package org.schlaagi.myproof
 
+import org.schlaagi.myproof.church_numerals.churchify
+import org.schlaagi.myproof.church_numerals.plus
+import org.schlaagi.myproof.church_numerals.unchurchify
 import org.schlaagi.myproof.lambdaterm.*
 
 object Test {
@@ -8,11 +11,32 @@ object Test {
         val x = Variable("x")
         val y = Variable("y")
         println("Variable x: $x")
-        println("Application of x to y: ${Application(x,y)}")
-        println("Forall x y: ${LambdaAbstraction(x,y)}")
-        println("Rename x to y in (x y): ${Application(x,y).renameVariable(x,y)}")
+        println("Application of x to y: ${Application(x, y)}")
+        println("Forall x y: ${LambdaAbstraction(x, y)}")
+        println("Rename x to y in ${Application(x, y)}: ${Application(x, y).renameVariable(x, y)}")
 
         val termToSubstitute = LambdaAbstraction(x, Application(y, x))
-        println("Substitute of $termToSubstitute is ${termToSubstitute.substitute(y, x)}")
+//        println("Substitute of $termToSubstitute is ${termToSubstitute.substitute(y, x)}")
+
+        val five = churchify(5)
+        println(five)
+        println(unchurchify(five))
+
+
+        println()
+        val plusOne = Application(plus, churchify(1))
+        println(plusOne)
+        println(plusOne.evaluate())
+        println(plusOne.evaluate().evaluate())
+        println()
+
+        val two = Application(
+            Application(plus, churchify(1)),
+            churchify(1)
+        )
+        println(two)
+        println(two.evaluate())
+        println(two.evaluate().evaluate())
+        println(two.evaluate().evaluate().evaluate())
     }
 }
---
+++ src/org/schlaagi/myproof/church_numerals/ChurchNumerals.kt
@@ -0,0 +1,70 @@
+package org.schlaagi.myproof.church_numerals
+
+import org.schlaagi.myproof.lambdaterm.Application
+import org.schlaagi.myproof.lambdaterm.LambdaAbstraction
+import org.schlaagi.myproof.lambdaterm.LambdaTerm
+import org.schlaagi.myproof.lambdaterm.Variable
+
+fun churchify(number: Int): LambdaTerm {
+    val variable = Variable("x")
+    val function = Variable("f")
+    return LambdaAbstraction(function, LambdaAbstraction(variable, applyRecursively(function, variable, number)))
+
+}
+
+fun applyRecursively(function: LambdaTerm, variable: Variable, numberOfTimes: Int): LambdaTerm {
+    if (numberOfTimes > 0) {
+        return Application(function, applyRecursively(function, variable, numberOfTimes - 1))
+    }
+    return variable
+}
+
+fun unchurchify(term: LambdaTerm): Int {
+    if (term is LambdaAbstraction) {
+        val f = term.variable
+        if (term.term is LambdaAbstraction) {
+            val x = term.term.variable
+            return unchirchifyRec(f, x, term.term.term)
+        }
+    }
+    throw IllegalArgumentException()
+}
+
+//ToDo: Rename
+fun unchirchifyRec(f: Variable, x: Variable, term: LambdaTerm): Int {
+    if (term == x) {
+        return 0
+    }
+    if (term is Application && term.first == f) {
+        return unchirchifyRec(f, x, term.second) + 1
+    }
+    throw IllegalArgumentException()
+}
+
+
+val plus: LambdaTerm
+    get() {
+        val m = Variable("m")
+        val n = Variable("n")
+        val f = Variable("f0")
+        val x = Variable("x0")
+        return LambdaAbstraction(
+            m,
+            LambdaAbstraction(
+                n,
+                LambdaAbstraction(
+                    f,
+                    LambdaAbstraction(
+                        x,
+                        Application(
+                            Application(m, f),
+                            Application(
+                                Application(n, f),
+                                x
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    }
